52d8ffc2efa986354fa3304fde8f13ae2f2a4599
Blaudio <bvargas_mont@hotmail.com>
2015-09-08 17:40:32 -0300

    Update Grafos.C

--- Grafos.C
+++ Grafos.C
@@ -1,133 +1,67 @@
 #include <stdio.h>
 #define Nodos 7
-#define Aristas 12
-
-typedef struct Vecino{
-  char nombre;
-  char color;//solo se usa en busqueda;
-  int distancia;//solo se usa en busqueda;
-  Vecino *vecinos;
-}Vecino
+//#define Aristas 12
+//aqui estan las conversiones entre las cordenadas de las tablas y los elementos
+// 0=A 1=B 2=C 3=D 4=E 5=F 6=G 7=H
 
 typedef struct Nodo{
-  char nombre;
-  char color;//solo se usa en busqueda;
-  int distancia;//solo se usa en busqueda;
-  int c_vecinos;//indica la cantidad de vecinos que tiene el nodo;
-  struct Nodo *Padre;//solo se usa en busqueda;
-  struct Nodos *vecinos[Nodos];
+  char nombre[Nodos];//son los nombres de los nodos;
+  char color;//solo se usa en busqueda,es para poder identificar los nodos visitados durante la busqueda;
+  int distancia;//solo se usa en busqueda,indica cuantos nodos se han recorrido desde el inicio;
+  int vecinos[Nodos][Nodos];//indica si son vecinos o no son vecinos los nodos X e Y;
+//  struct Nodo *Padre;//solo se usa en busqueda;
 }Nodo;
 
 Nodo *inicializa(Nodo grafo){
+  int contadorA,contadorB
   Nodo *aux = (Nodo *)malloc(sizeof(Nodo));
-  aux->Nombre=NULL;
-  aux->color=W;
-  aux->distancia=0;
-  aux->padre=NULL;
-  aux->c_vecinos=0;
-  aux->vecinos[0]=NULL;
+  for(contadorA=0; contadorA<=Nodos; contadorA++){
+     aux->nombre[contadorA]=NULL;
+     aux->color[contadorA]=("W");//se le dara el color W(white/blanco)de manera predeterminada a todos;
+     aux->distancia[contadorA]=0;//como no se ha iniciado una busqueda la distancia recorrida sera 0;
+    for(contadorB=0; contadorB<=Nodos; contadorB++){
+      aux->vecinos[contadorA][contadorB]=0;//ninguno nodo sera vecino de manera predeterminada
+    }
+  }
   return (aux);
 }
 
 //if(grafo->nombre=="A"){} //base para no tener que crear mas de un vecino
 
-Nodo *agrega_elementos_A(Nodo grafo){
-  grafo->nombre=("A");
-  grafo->c_vecinos=2;
-  grafo->vecinos[0]=*agrega_elementos_B(grafo);
-  grafo->vecinos[1]=//*agrega_elementos_C(grafo);
-  grafo->vecinos[2]=//*agrega_elementos_F(grafo);
-  return(grafo);
+Nodo *agrega_elementos(Nodo grafo){
+  grafo->nombre[0]=("A");
+  grafo->nombre[1]=("B");
+  grafo->nombre[2]=("C");
+  grafo->nombre[3]=("D");
+  grafo->nombre[4]=("E");
+  grafo->nombre[5]=("F");
+  grafo->nombre[6]=("G");
+  grafo->nombre[7]=("H");
+  grafo->vecinos[0][1]=1;
+  grafo->vecinos[0][2]=1;
+  grafo->vecinos[0][5]=1;
+  grafo->vecinos[1][0]=1;
+  grafo->vecinos[1][2]=1;
+  grafo->vecinos[1][7]=1;
+  grafo->vecinos[2][0]=1;
+  grafo->vecinos[2][1]=1;
+  grafo->vecinos[2][3]=1;
+  grafo->vecinos[2][4]=1;
+  grafo->vecinos[3][2]=1;
+  grafo->vecinos[3][5]=1;
+  grafo->vecinos[3][6]=1;
+  grafo->vecinos[4][2]=1;
+  grafo->vecinos[4][6]=1;
+  grafo->vecinos[4][7]=1;
+  grafo->vecinos[5][0]=1;
+  grafo->vecinos[5][3]=1;
+  grafo->vecinos[5][6]=1;
+  grafo->vecinos[6][3]=1;
+  grafo->vecinos[6][4]=1;
+  grafo->vecinos[6][5]=1;
+  grafo->vecinos[6][7]=1;
+  grafo->vecinos[7][1]=1;
+  grafo->vecinos[7][4]=1;
+  grafo->vecinos[7][6]=1;
+  return (grafo);
 }
-Nodo *agrega_elementos_B(Nodo grafo){
-  Nodo*aux;
-  aux->nombre=("B");
-  aux->c_vecinos=2;
-  if(grafo->nombre=="A"){
-    aux->vecinos[0]=grafo;  
-  }
-  aux->vecinos[1]=*agrega_elementos_C(grafo);
-  aux->vecinos[2]=//*agrega_elementos_H(grafo);
-  return(aux);
-}
-Nodo *agrega_elementos_C(Nodo grafo){
-  Nodo*aux;
-  aux->nombre=("C");
-  aux->c_vecinos=3;
-  if(aux->nombre=="A"){
-    aux->vecinos[0]=grafo;
-  }
-  if(aux->nombre=="B"){
-    aux->vecinos[1]=grafo;
-  }
-  aux->vecinos[2]=*agrega_elementos_D(grafo);
-  aux->vecinos[3]=//*agrega_elementos_E(grafo);
-  return(grafo);
-}
-Nodo *agrega_elementos_D(Nodo grafo){
-  grafo->nombre=("D");
-  grafo->c_vecinos=2;
-  grafo->vecinos[0]=//*agrega_elementos_c(grafo);
-  grafo->vecinos[1]=*agrega_elementos_F(grafo);
-  grafo->vecinos[2]=//*agrega_elementos_G(grafo);
-  return(grafo);
-}
-
-Nodo *agrega_elementos_F(Nodo grafo){
-  grafo->nombre=("F");
-  grafo->c_vecinos=2;
-  if(grafo->nombre=="A"){
-    grafo->vecinos[0]=grafo;
-  }
-  grafo->vecinos[1]=//*agrega_elementos_D(grafo);
-  grafo->vecinos[2]=*agrega_elementos_G(grafo);
-  return(grafo);
-}
-Nodo *agrega_elementos_G(Nodo grafo){
-  grafo->nombre=("G");
-  grafo->c_vecinos=3;
-  grafo->vecinos[0]=//*agrega_elementos_F(grafo);
-  grafo->vecinos[1]=//*agrega_elementos_D(grafo);
-  grafo->vecinos[2]=*agrega_elementos_E(grafo);
-  grafo->vecinos[3]=//*agrega_elementos_H(grafo);
-  return(grafo);
-}
-Nodo *agrega_elementos_E(Nodo grafo){
-  grafo->nombre=("E");
-  grafo->c_vecinos=2;
-  grafo->vecinos[0]=//*agrega_elementos_C(grafo);
-  grafo->vecinos[1]=//*agrega_elementos_G(grafo);
-  grafo->vecinos[2]=*agrega_elementos_H(grafo);
-  return(grafo);
-}
-Nodo *agrega_elementos_H(Nodo grafo){
-  grafo->nombre=("H");
-  grafo->c_vecinos=2;
-  grafo->vecinos[0]=//*agrega_elementos_C(grafo);
-  grafo->vecinos[1]=//*agrega_elementos_G(grafo);
-  grafo->vecinos[2]=//*agrega_elementos_H(grafo);
-  return(grafo);
-}
-
-//#define TE 5
-//#define Nulo NULL
-
-//typedef char  *tetq;
-//typedef float tvalor;
-
-//typedef struct arco {
-//	struct nodo *origen;
-//	struct nodo *destino;
-//	tvalor valor;
-//	struct arco *sig;
-//} *tarco;
-
-//typedef struct nodo {
-//	int nodo;
-//	tetq etiq;
-//	tarco ady;
-//	tarco inc;
-//	struct nodo *sig;
-//} *tnodo;
-
-//typedef tnodo tgrafo;
