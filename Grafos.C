8749f1fe240d4d2c713493176c89573fedff9775
Blaudio <bvargas_mont@hotmail.com>
2015-09-08 08:53:25 -0300

    Update Grafos.C

--- Grafos.C
+++ Grafos.C
@@ -1,10 +1,50 @@
 #include <stdio.h>
-//#define Nodos 8
-//#define Aristas 13
+#define Nodos 7
+#define Aristas 12
+
+typedef struct Vecino{
+  char nombre;
+  char color;
+  int distancia;
+  Vecino *vecinos;
+}Vecino
 
 typedef struct Nodo{
   char nombre;
   char color;
   int distancia;
-  struct DFS *Padre;
+  struct Nodo *Padre;
+  struct Vecino *vecinos;
 }Nodo;
+
+Nodo *inicializa(Nodo grafo){
+  Nodo *aux = (Nodo *)malloc(sizeof(Nodo));
+  aux->Nombre=NULL;
+  aux->color=W;
+  aux->distancia=0:
+  aux->padre=NULL;
+  aux->vecinos=NULL;
+}
+
+//#define TE 5
+//#define Nulo NULL
+
+//typedef char  *tetq;
+//typedef float tvalor;
+
+//typedef struct arco {
+//	struct nodo *origen;
+//	struct nodo *destino;
+//	tvalor valor;
+//	struct arco *sig;
+//} *tarco;
+
+//typedef struct nodo {
+//	int nodo;
+//	tetq etiq;
+//	tarco ady;
+//	tarco inc;
+//	struct nodo *sig;
+//} *tnodo;
+
+//typedef tnodo tgrafo;
