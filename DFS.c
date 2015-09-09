d277094b986c375307690bf149bab08c6febedca
Blaudio <bvargas_mont@hotmail.com>
2015-09-09 19:07:46 -0300

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "Grafos.c"
 
-Nodo Busqueda_profundidad(Nodo *Grafo){
-  for
+Nodo Busqueda_profundidad(Nodo *Grafo,  int elemento){
+  Nodo *aux = *Grafo;
+  for(contador=0; contador<=Nodos; contador++){
+    if(aux->color==("W") &&  grafo->vecinos[elemento][contador]==1){
+      Busqueda_profundidad_visita(aux, contador);
+    }
+  }  
+}
+
+Nodo Busqueda_profundidad_visita(Nodo *Grafo,  int elemento){
+  
 }
