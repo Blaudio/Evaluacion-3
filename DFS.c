b5600ab24de68c1cd06600eca6b490ca0ca512a3
Blaudio <bvargas_mont@hotmail.com>
2015-09-11 17:00:58 -0300

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -5,11 +5,13 @@ Nodo Busqueda_profundidad(Nodo *Grafo,  int elemento){
   Nodo *aux = *Grafo;
   int contador;
   contador=0;
+  printf ("|%d",aux->nombre[elemento_actual]);//Aqui se imprime el estado de busqueda.
   for(contador=0; contador<=Nodos; contador++){
     if(aux->color==("W") &&  grafo->vecinos[elemento][contador]==1){
       Busqueda_profundidad_visita(aux, contador);
     }
   }  
+  printf ("|\n");//Aquie se finaliza la impresion de la busqueda del nodo actual
 }
 
 Nodo Busqueda_profundidad_visita(Nodo *Grafo,  int elemento){
@@ -20,6 +22,7 @@ Nodo Busqueda_profundidad_visita(Nodo *Grafo,  int elemento){
   Grafo->color[elemento]=("G");
   for(contador=0; contador<=Nodos; contador++){
     if(aux->color==("W") &&  grafo->vecinos[elemento][contador]==1){
+      printf ("->%d",aux->nombre[contador]);//Aqui se imprime el estado de busqueda para el nodo seleccionado.
       Grafo->padre[contador]=elemento;
       Busqueda_profundidad_visita(aux, contador);
     }
