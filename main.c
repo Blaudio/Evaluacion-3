09b25fb4d12a6c41492e0c141ee4a2b445a49572
Blaudio <bvargas_mont@hotmail.com>
2015-10-18 18:46:17 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BFS.h"
+//#include "DFS.h"
 
+int main(){
+	Nodo *grafo;
+	grafo=inicializa();
+	grafo=agrega_elementos(grafo);
+	Busqueda_a_lo_ancho(grafo,3);
+	//Busqueda_profundidad(grafo,0);
+}
