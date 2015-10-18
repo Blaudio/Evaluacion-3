87c77c4ba70c0ce130357e774e527d2257d151d2
Blaudio <bvargas_mont@hotmail.com>
2015-10-18 19:52:20 -0300

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Grafos.h"
-
-Nodo Busqueda_profundidad(Nodo *Grafo,  int elemento){
-  Nodo *aux = Grafo;
-  int contador;
-  contador=0;
-  printf ("|%d",aux->nombre[elemento]);//Aqui se imprime el estado de busqueda.
-  for(contador=0; contador<=Nodos; contador++){
-    if(aux->color[elemento]==("W") &&  aux->vecinos[elemento][contador]==1){
-      Busqueda_profundidad_visita(aux, contador);
-    }
-  }  
-  printf ("|\n");//Aquie se finaliza la impresion de la busqueda del nodo actual
-}
 
 int Busqueda_profundidad_visita(Nodo *Grafo,  int elemento){
   int Tiempo, contador;
@@ -21,13 +9,29 @@ int Busqueda_profundidad_visita(Nodo *Grafo,  int elemento){
   Grafo->tiempo=Tiempo;
   Grafo->color[elemento]=("G");
   for(contador=0; contador<=Nodos; contador++){
-    if(Grafo->color==("W") &&  Grafo->vecinos[elemento][contador]==1){
-      printf ("->%d",Grafo->nombre[contador]);//Aqui se imprime el estado de busqueda para el nodo seleccionado.
-      Grafo->padre[contador]=elemento;
-      Busqueda_profundidad_visita(Grafo, contador);
+    if(Grafo->color[contador] == ("W")){
+    	if(Grafo->vecinos[elemento][contador]==1){
+    		printf ("->%s",Grafo->nombre[contador]);//Aqui se imprime el estado de busqueda para el nodo seleccionado.
+    		Grafo->padre[contador]=elemento;
+    		Grafo->color[contador]=("B");
+    		Busqueda_profundidad_visita(Grafo, contador);
+    	}
     }
   }  
   Grafo->color[elemento]=("B");
   Tiempo=Tiempo+1;
   Grafo->tiempo=Tiempo;
+}
+
+Nodo Busqueda_profundidad(Nodo *Grafo,  int elemento){
+  Nodo *aux = Grafo;
+  int contador;
+  contador=0;
+  printf ("|%s",aux->nombre[elemento]);//Aqui se imprime el estado de busqueda.
+  for(contador=0; contador<=Nodos; contador++){
+    if(aux->color[elemento]==("W") &&  aux->vecinos[elemento][contador]==1){
+      Busqueda_profundidad_visita(aux, contador);
+    }
+  }  
+  printf ("|\n");//Aquie se finaliza la impresion de la busqueda del nodo actual
 }
