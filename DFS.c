58882d23cd3ea5eaa7a16f1279543c9aaac5a986
Blaudio <bvargas_mont@hotmail.com>
2015-10-18 18:45:57 -0300

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
-#include "Grafos.c"
+#include "Grafos.h"
 
 Nodo Busqueda_profundidad(Nodo *Grafo,  int elemento){
-  Nodo *aux = *Grafo;
+  Nodo *aux = Grafo;
   int contador;
   contador=0;
-  printf ("|%d",aux->nombre[elemento_actual]);//Aqui se imprime el estado de busqueda.
+  printf ("|%d",aux->nombre[elemento]);//Aqui se imprime el estado de busqueda.
   for(contador=0; contador<=Nodos; contador++){
-    if(aux->color==("W") &&  grafo->vecinos[elemento][contador]==1){
+    if(aux->color[elemento]==("W") &&  aux->vecinos[elemento][contador]==1){
       Busqueda_profundidad_visita(aux, contador);
     }
   }  
   printf ("|\n");//Aquie se finaliza la impresion de la busqueda del nodo actual
 }
 
-Nodo Busqueda_profundidad_visita(Nodo *Grafo,  int elemento){
+int Busqueda_profundidad_visita(Nodo *Grafo,  int elemento){
   int Tiempo, contador;
   contador=0;
-  Tiempo=grafo->tiempo+1;
+  Tiempo=Grafo->tiempo+1;
   Grafo->tiempo=Tiempo;
   Grafo->color[elemento]=("G");
   for(contador=0; contador<=Nodos; contador++){
-    if(aux->color==("W") &&  grafo->vecinos[elemento][contador]==1){
-      printf ("->%d",aux->nombre[contador]);//Aqui se imprime el estado de busqueda para el nodo seleccionado.
+    if(Grafo->color==("W") &&  Grafo->vecinos[elemento][contador]==1){
+      printf ("->%d",Grafo->nombre[contador]);//Aqui se imprime el estado de busqueda para el nodo seleccionado.
       Grafo->padre[contador]=elemento;
-      Busqueda_profundidad_visita(aux, contador);
+      Busqueda_profundidad_visita(Grafo, contador);
     }
   }  
   Grafo->color[elemento]=("B");
